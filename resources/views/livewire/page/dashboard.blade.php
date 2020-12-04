04284ad7921da9ec5843f3ceee9c27284dd49b79
safwan <safwan@csc.net.my>
2020-12-04 11:03:03 +0800

    dashboard page

---
+++ resources/views/livewire/page/dashboard.blade.php
@@ -0,0 +1,136 @@
+<div class="grid grid-cols-12 gap-6">
+    <div class="grid grid-cols-12 col-span-12 gap-6 xxl:col-span-9">
+        <div class="col-span-12 mt-8">
+            <div class="flex items-center h-10 intro-y">
+                <h2 class="mr-5 text-lg font-medium truncate">General Report</h2>
+            </div>
+            <div class="grid grid-cols-12 gap-6 mt-5">
+                <x-dashboard.info-card bg="white" title="item Sales" value="4.510" percentage="30%" percentageBg="green" cardRoute="#" >
+                    <x-slot name="svg">
+                        <x-heroicon-o-shopping-cart class="h-7 w-7 text-blue-400"/>
+                    </x-slot>
+                </x-dashboard.info-card>
+
+                <x-dashboard.info-card bg="white" title="New Orders" value="3.521" percentage="2%" percentageBg="red" cardRoute="#" >
+                    <x-slot name="svg">
+                        <x-heroicon-o-desktop-computer class="h-7 w-7 text-yellow-400"/>
+                    </x-slot>
+                </x-dashboard.info-card>
+
+                <x-dashboard.info-card bg="white" title="Total Products" value="2.145" percentage="12%" percentageBg="green" cardRoute="#" >
+                    <x-slot name="svg">
+                        <x-heroicon-o-desktop-computer class="h-7 w-7 text-yellow-400"/>
+                    </x-slot>
+                </x-dashboard.info-card>
+
+                <x-dashboard.info-card bg="white" title="Total Visitor" value="152.00" percentage="22%" percentageBg="green" cardRoute="#" >
+                    <x-slot name="svg">
+                        <x-heroicon-o-user class="h-7 w-7 text-green-400"/>
+                    </x-slot>
+                </x-dashboard.info-card> 
+            </div>
+        </div>
+        <div class="col-span-12 mt-5">
+            <div class="grid gap-2 lg:grid-cols-1 sm:grid-cols-1">
+                <div class="bg-white p-4" id="chartline"></div>
+            </div>
+        </div>
+        <div class="col-span-12 mt-5">
+            <div class="grid gap-2 lg:grid-cols-2 sm:grid-cols-1">
+                <div class="bg-white p-4" id="chartpie"></div>
+
+                <div class="bg-white p-4">
+                    <h1 class="font-bold text-base">Weekly Top Seller</h1>
+                    <div class="mt-4 flex justify-center">
+                        <x-table.table>
+                            <x-slot name="thead">
+                                <x-table.table-header class="text-left" value="PRODUCT NAME" sort="" livewire=""/>
+                                <x-table.table-header class="text-left" value="STOCK" sort="" livewire=""/>
+                                <x-table.table-header class="text-left" value="STATUS" sort="" livewire=""/>
+                                <x-table.table-header class="text-left" value="ACTIONS" sort="" livewire=""/>
+                            </x-slot>
+                            <x-slot name="tbody">
+                                <tr>
+                                    <x-table.table-body colspan="" class=" text-sm font-medium text-gray-700">
+                                        <p>Apple MacBook Pro 13</p>
+                                        <p class="text-xs text-gray-400">PC & Laptop</p>
+                                    </x-table.table-body>
+                                    <x-table.table-body colspan="" class=" text-sm font-medium text-gray-700">
+                                        <p>77</p>
+                                    </x-table.table-body>
+                                    <x-table.table-body colspan="" class=" text-sm font-medium text-gray-700">
+                                        <div class="flex text-green-500">
+                                            <x-heroicon-o-check-circle class="w-5 h-5 mr-1"/>
+                                            <p>Active</p>
+                                        </div>
+                                    </x-table.table-body>
+                                    <x-table.table-body colspan="" class=" text-sm font-medium text-gray-700">
+                                        <div class="flex">
+                                            <x-heroicon-o-pencil-alt class="w-5 h-5 mr-1 text-blue-500"/>
+                                            <p class="text-blue-500">Edit</p>
+
+                                            <x-heroicon-o-trash class="w-5 h-5 mr-1 ml-3 text-red-500"/>
+                                            <p class="text-red-500">Delete</p>
+                                        </div>
+                                    </x-table.table-body>
+                                </tr>
+                                <tr>
+                                    <x-table.table-body colspan="" class=" text-sm font-medium text-gray-700">
+                                        <p>Oppo Find X2 Pro</p>
+                                        <p class="text-xs text-gray-400">Smartphone & Tablet</p>
+                                    </x-table.table-body>
+                                    <x-table.table-body colspan="" class=" text-sm font-medium text-gray-700">
+                                        <p>50</p>
+                                    </x-table.table-body>
+                                    <x-table.table-body colspan="" class=" text-sm font-medium text-gray-700">
+                                        <div class="flex text-green-500">
+                                            <x-heroicon-o-check-circle class="w-5 h-5 mr-1"/>
+                                            <p>Active</p>
+                                        </div>
+                                    </x-table.table-body>
+                                    <x-table.table-body colspan="" class=" text-sm font-medium text-gray-700">
+                                        <div class="flex">
+                                            <x-heroicon-o-pencil-alt class="w-5 h-5 mr-1 text-blue-500"/>
+                                            <p class="text-blue-500">Edit</p>
+
+                                            <x-heroicon-o-trash class="w-5 h-5 mr-1 ml-3 text-red-500"/>
+                                            <p class="text-red-500">Delete</p>
+                                        </div>
+                                    </x-table.table-body>
+                                </tr>
+                                <tr>
+                                    <x-table.table-body colspan="" class=" text-sm font-medium text-gray-700">
+                                        <p>Dell XPS 13</p>
+                                        <p class="text-xs text-gray-400">PC & Laptop</p>
+                                    </x-table.table-body>
+                                    <x-table.table-body colspan="" class=" text-sm font-medium text-gray-700">
+                                        <p>100</p>
+                                    </x-table.table-body>
+                                    <x-table.table-body colspan="" class=" text-sm font-medium text-gray-700">
+                                        <div class="flex text-green-500">
+                                            <x-heroicon-o-check-circle class="w-5 h-5 mr-1"/>
+                                            <p>Active</p>
+                                        </div>
+                                    </x-table.table-body>
+                                    <x-table.table-body colspan="" class=" text-sm font-medium text-gray-700">
+                                        <div class="flex">
+                                            <x-heroicon-o-pencil-alt class="w-5 h-5 mr-1 text-blue-500"/>
+                                            <p class="text-blue-500">Edit</p>
+
+                                            <x-heroicon-o-trash class="w-5 h-5 mr-1 ml-3 text-red-500"/>
+                                            <p class="text-red-500">Delete</p>
+                                        </div>
+                                    </x-table.table-body>
+                                </tr>
+                            </x-slot>
+                        </x-table.table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> 
+
+
+
+
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,6 +1,123 @@
 @extends('default.default')
 @section('content')
+    <livewire:page.dashboard/>
 
-<h1>Dashboard</h1>
 
+{{-- lineChart --}}
+<script>
+    var chart = document.querySelector('#chartline')
+    var options = {
+        chart: {
+            height: 350,
+            type: "line",
+            zoom: {
+                enabled: false
+            }
+        },
+        dataLabels: {
+            enabled: false
+        },
+        colors: ["#FF1654", "#247BA0"],
+        series: [
+            {
+            name: "This Month RM 14,000",
+            data: [0, 200, 400, 300, 500, 200, 100, 800,1000,200,150,1200]
+            },
+            {
+            name: "Last Month 9,000",
+            data: [10, 400, 500, 200, 300, 100, 200, 500,100,200,150,1000]
+            }
+        ],
+        stroke: {
+            width: [4, 4],
+            curve: 'smooth',
+        },
+        plotOptions: {
+            bar: {
+            columnWidth: "20%"
+            }
+        },
+        xaxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug','Sep','Oct','Nov','Dec']
+        },
+        yaxis: [
+            {
+                axisTicks: {
+                    show: true
+                },
+                axisBorder: {
+                    show: true,
+                    color: "#FF1654"
+                },
+                labels: {
+                    style: {
+                    colors: "#FF1654"
+                    }
+                },
+            },
+            {
+                opposite: true,
+                axisTicks: {
+                    show: true
+                },
+                axisBorder: {
+                    show: true,
+                    color: "#247BA0"
+                },
+                labels: {
+                    style: {
+                    colors: "#247BA0"
+                    }
+                },
+            }
+        ],
+        tooltip: {
+            shared: false,
+            intersect: true,
+            x: {
+            show: false
+            }
+        },
+        title: {
+            text: 'Weekly Top Seller',
+            align: 'left'
+        },
+        legend: {
+            horizontalAlign: "left",
+            offsetX: 40
+        }
+    };
+        var chart = new ApexCharts(chart, options);
+        chart.render();
+</script>
+{{-- lineChart --}}
+
+{{-- pie Chart --}}
+<script>
+    var chart = document.querySelector('#chartpie')
+    var options = {
+            series: [44, 55, 13,],
+            chart: {
+            height: 350,
+            type: 'pie',
+            },
+            title: {
+                text: 'Weekly Top Seller',
+                align: 'left'
+            },
+            labels: ['17 - 30 Years old', '31 - 50 Years old', '>= 50 Years old'],
+            responsive: [{
+            breakpoint: 480,
+            options: {
+                legend: {
+                position: 'bottom'
+                }
+            }
+            }]
+        };
+        var chart = new ApexCharts(chart, options);
+        chart.render();
+</script>
+{{-- pie Chart --}}
 @endsection
+
