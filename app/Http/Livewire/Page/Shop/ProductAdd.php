f9b5d6d90c25b36ceaba2158bdc9c3866f26c74f
safwan <safwan@csc.net.my>
2021-02-22 14:49:45 +0800

    add page add & edit product page in shop module

---
+++ app/Http/Livewire/Page/Shop/ProductAdd.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Page\Shop;
+
+use Livewire\Component;
+
+class ProductAdd extends Component
+{
+    public function render()
+    {
+        return view('livewire.page.shop.product-add');
+    }
+}
