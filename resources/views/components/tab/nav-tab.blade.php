ebf5150d45cf643e1704263ae396f223d58fd84e
Nazirul Fitri Fauzi <nazirulfitrifauzi@gmail.com>
2021-03-30 18:31:17 +0800

    fix stock management

--- resources/views/components/tab/nav-tab.blade.php
+++ resources/views/components/tab/nav-tab.blade.php
@@ -1,6 +1,6 @@
 
 <nav class="flex flex-col sm:flex-row">
-    <div class="text-gray-600 py-2 px-6 block focus:outline-none cursor-pointer" x-on:click.prevent="active = {{ $name }}" x-bind:class="{'text-yellow-400 border-b-4 border-yellow-400 ': active === {{ $name }} }"
+    <div class="block px-6 py-2 text-gray-600 cursor-pointer focus:outline-none" x-on:click.prevent="active = {{ $name }}" x-bind:class="{'text-yellow-400 border-b-4 border-yellow-400 ': active === {{ $name }} }"
     {{ $livewire }}
     >
         <div class="">
@@ -8,5 +8,3 @@
         </div>
     </div>
 </nav>
-
-
