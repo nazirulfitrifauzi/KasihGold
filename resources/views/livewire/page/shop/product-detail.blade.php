3429b019fcecc4afde53b570463d8f4379f43a08
Nazirul Fitri Fauzi <nazirulfitrifauzi@gmail.com>
2021-05-07 12:36:31 +0800

    fix image streching issue

--- resources/views/livewire/page/shop/product-detail.blade.php
+++ resources/views/livewire/page/shop/product-detail.blade.php
@@ -1,14 +1,13 @@
 <div>
-    <div class="py-6 bg-white my-10">
-        <div class="w-full px-4 sm:px-6 lg:px-8 mt-1">
+    <div class="py-6 my-10 bg-white">
+        <div class="w-full px-4 mt-1 sm:px-6 lg:px-8">
             <div class="px-1 mb-8">
                 <div class="flex justify-between">
-                    <div class="text-gray-700 md:text-2xl font-semibold">
+                    <div class="font-semibold text-gray-700 md:text-2xl">
                         Product Detail
                     </div>
                     <div>
-                        <a href="{{route('product-view')}}" class="cursor-pointer flex items-center px-4 py-1 
-                            text-sm font-bold text-white bg-yellow-400 rounded  focus:outline-none hover:bg-yellow-300">
+                        <a href="{{route('product-view')}}" class="flex items-center px-4 py-1 text-sm font-bold text-white bg-yellow-400 rounded cursor-pointer focus:outline-none hover:bg-yellow-300">
                             <x-heroicon-o-arrow-circle-left class="w-5 h-5 mr-2 text-white" />
                             Product List
                         </a>
@@ -16,41 +15,41 @@
                 </div>
             </div>
             <div class="container">
-                <div class="flex flex-col md:flex-row -mx-4">
+                <div class="flex flex-col -mx-4 md:flex-row">
                     <!-Start detail of image -->
-                    <div class="md:flex-1 px-4">
+                    <div class="px-4 md:flex-1">
                         <div x-data="{imageUrl: '{{ asset('storage/'.$info->prod_img1) }}'}" x-cloak>
-                            <div class="h-64 md:h-80 rounded-lg bg-gray-100 mb-4">
-                                <div class="h-64 md:h-80 rounded-lg bg-gray-100 mb-4 flex items-center justify-center">
-                                    <img id="main" :src="imageUrl" class="h-full w-full bg-cover" />
+                            <div class="h-64 mb-4 bg-gray-100 rounded-lg md:h-80">
+                                <div class="flex items-center justify-center h-64 mb-4 bg-gray-100 rounded-lg md:h-80">
+                                    <img id="main" :src="imageUrl" class="object-contain w-full h-full bg-cover" />
                                 </div>
                             </div>
-                            <div class="grid gap-6 grid-cols-4 sm:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4 mt-6">
-                                <button class=" focus:outline-none w-full rounded-lg h-24 md:h-32 bg-gray-100 flex items-center justify-center">
+                            <div class="grid grid-cols-4 gap-6 mt-6 sm:grid-cols-4 lg:grid-cols-4 xl:grid-cols-4">
+                                <button class="flex items-center justify-center w-full h-24 bg-gray-100 rounded-lg focus:outline-none md:h-32">
                                     <img src="{{ asset('storage/'.$info->prod_img1) }}"
-                                        class="h-full w-full bg-cover"
-                                        @click="imageUrl = '{{ asset('storage/'.$info->prod_img1) }}'" 
+                                        class="object-contain w-full h-full bg-cover"
+                                        @click="imageUrl = '{{ asset('storage/'.$info->prod_img1) }}'"
                                         />
                                 </button>
                                 <button
-                                    class="focus:outline-none w-full rounded-lg h-24 md:h-32 bg-gray-100 flex items-center justify-center">
-                                    <img src="{{ asset('storage/'.$info->prod_img2) }}" 
-                                        class="h-full w-full bg-cover"
-                                        @click="imageUrl = '{{ asset('storage/'.$info->prod_img2) }}'" 
+                                    class="flex items-center justify-center w-full h-24 bg-gray-100 rounded-lg focus:outline-none md:h-32">
+                                    <img src="{{ asset('storage/'.$info->prod_img2) }}"
+                                        class="object-contain w-full h-full bg-cover"
+                                        @click="imageUrl = '{{ asset('storage/'.$info->prod_img2) }}'"
                                         />
                                 </button>
                                 <button
-                                    class="focus:outline-none w-full rounded-lg h-24 md:h-32 bg-gray-100 flex items-center justify-center">
-                                    <img src="{{ asset('storage/'.$info->prod_img3) }}" 
-                                        class="h-full w-full bg-cover"
-                                        @click="imageUrl = '{{ asset('storage/'.$info->prod_img3) }}'" 
+                                    class="flex items-center justify-center w-full h-24 bg-gray-100 rounded-lg focus:outline-none md:h-32">
+                                    <img src="{{ asset('storage/'.$info->prod_img3) }}"
+                                        class="object-contain w-full h-full bg-cover"
+                                        @click="imageUrl = '{{ asset('storage/'.$info->prod_img3) }}'"
                                         />
                                 </button>
                                 <button
-                                    class="focus:outline-none w-full rounded-lg h-24 md:h-32 bg-gray-100 flex items-center justify-center">
-                                    <img src="{{ asset('storage/'.$info->prod_img4) }}" 
-                                        class="h-full w-full bg-cover"
-                                        @click="imageUrl = '{{ asset('storage/'.$info->prod_img4) }}'" 
+                                    class="flex items-center justify-center w-full h-24 bg-gray-100 rounded-lg focus:outline-none md:h-32">
+                                    <img src="{{ asset('storage/'.$info->prod_img4) }}"
+                                        class="object-contain w-full h-full bg-cover"
+                                        @click="imageUrl = '{{ asset('storage/'.$info->prod_img4) }}'"
                                         />
                                 </button>
                             </div>
@@ -59,20 +58,20 @@
                     <!--End detail of image -->
 
                     <!--Start detail for buying -->
-                    <div class="mt-8 lg:flex-1 px-4 md:mt-0">
-                        <h2 class="mb-2 leading-tight tracking-tight font-bold text-gray-800 text-2xl md:text-3xl">{{$info->prod_name}}</h2>
-                        <p class="text-gray-500 text-sm">By 
+                    <div class="px-4 mt-8 lg:flex-1 md:mt-0">
+                        <h2 class="mb-2 text-2xl font-bold leading-tight tracking-tight text-gray-800 md:text-3xl">{{$info->prod_name}}</h2>
+                        <p class="text-sm text-gray-500">By
                             <span class="text-yellow-400">{{$userInfo->name}}</span>
                         </p>
-                        <div class="flex items-center space-x-4 my-4">
+                        <div class="flex items-center my-4 space-x-4">
                             <div>
-                                <div class="rounded-lg bg-gray-100 flex py-2 px-3">
-                                    <span class="font-bold text-yellow-400 text-xl">{{$info->prod_price}}</span>
+                                <div class="flex px-3 py-2 bg-gray-100 rounded-lg">
+                                    <span class="text-xl font-bold text-yellow-400">{{$info->prod_price}}</span>
                                 </div>
                             </div>
                             <div class="flex-1">
-                                <p class="text-green-500 text-xl font-semibold">Save 12%</p>
-                                <p class="text-gray-400 text-sm">Inclusive of all Taxes.</p>
+                                <p class="text-xl font-semibold text-green-500">Save 12%</p>
+                                <p class="text-sm text-gray-400">Inclusive of all Taxes.</p>
                             </div>
                         </div>
                         <p class="text-gray-500">{{$info->prod_desc}}
@@ -83,13 +82,13 @@
                                     <div class="relative">
                                         <div>
                                             <select value="prod_qty" wire:model="prod_qty" default="yes"
-                                                class="cursor-pointer appearance-none rounded-xl border border-gray-200 pl-4 pr-8 pt-5 h-14 flex items-end pb-1">
+                                                class="flex items-end pt-5 pb-1 pl-4 pr-8 border border-gray-200 appearance-none cursor-pointer rounded-xl h-14">
                                                 @for ($i = 1; $i <= 10; $i++)
                                                 <option value="{{$i}}">{{$i}}</option>
                                                 @endfor
                                             </select>
 
-                                            <svg class="w-5 h-5 text-gray-400 absolute right-0 bottom-0 mb-2 mr-2"
+                                            <svg class="absolute bottom-0 right-0 w-5 h-5 mb-2 mr-2 text-gray-400"
                                                 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                 stroke="currentColor">
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -97,22 +96,22 @@
                                             </svg>
                                         </div>
                                         <div
-                                            class="text-center top-0 px-4 py-1 absolute block text-xs uppercase text-gray-400 tracking-wide font-semibold">
+                                            class="absolute top-0 block px-4 py-1 text-xs font-semibold tracking-wide text-center text-gray-400 uppercase">
                                             Qty
                                         </div>
                                     </div>
-                                    
+
                                     <div class="flex">
                                         <a href="{{route('product-buy',['iid'=>$info->id])}}"
-                                            class="cursor-pointer items-center px-6 py-4 font-semibold rounded-xl bg-green-400 hover:bg-green-300 text-white focus:outline-none">
+                                            class="items-center px-6 py-4 font-semibold text-white bg-green-400 cursor-pointer rounded-xl hover:bg-green-300 focus:outline-none">
                                             Buy Now
                                         </a>
                                         {{-- <button type="submit"
-                                            class="cursor-pointer items-center px-6 py-4 font-semibold rounded-xl bg-green-400 hover:bg-green-300 text-white focus:outline-none">
+                                            class="items-center px-6 py-4 font-semibold text-white bg-green-400 cursor-pointer rounded-xl hover:bg-green-300 focus:outline-none">
                                             Buy Now
                                         </button> --}}
                                         <button type="button" @if ($prod_qty==null) wire:click="addCart({{1}})" @else wire:click="addCart({{$prod_qty}})" @endif
-                                            class="ml-2 h-14 px-6 py-2 font-semibold rounded-xl bg-yellow-400 hover:bg-yellow-300 text-white focus:outline-none">
+                                            class="px-6 py-2 ml-2 font-semibold text-white bg-yellow-400 h-14 rounded-xl hover:bg-yellow-300 focus:outline-none">
                                             Add to Cart
                                         </button>
                                     </div>
@@ -121,7 +120,7 @@
                         </x-form.basic-form>
                     </div>
                     <!--End detail for buying -->
-                    
+
                 </div>
             </div>
         </div>
