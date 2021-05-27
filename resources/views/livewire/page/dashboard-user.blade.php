4315c0c937a3334a2b8b1ec8c8772567d3c04a8a
safwan <safwan@csc.net.my>
2021-05-27 12:23:00 +0800

    edit user dashboard

--- resources/views/livewire/page/dashboard-user.blade.php
+++ resources/views/livewire/page/dashboard-user.blade.php
@@ -26,56 +26,107 @@
         </div>
         <div class="col-span-12 lg:col-span-12 xxl:col-span-12">
             <div class="grid grid-cols-12 gap-6">
-                <div class="col-span-12 lg:col-span-6 xxl:col-span-6 rounded-lg" >
-                    <div class= "w-full lg:flex p-4  shadow-lg">
-                        <div class="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden" 
-                        style="background-image: url('https://mocah.org/uploads/posts/519166-business-global.jpg')">
+                <div class="relative col-span-12 lg:col-span-6 xxl:col-span-6 bg-white shadow-xl rounded-lg">
+                    <div>
+                        <h1 class="bg-yellow-400 p-4 absolute top-0 w-full text-white rounded-t-lg font-semibold text-lg">News</h1>
+                        <div class="mt-14 ">
+                            <h1 class="font-semibold text-lg p-4">Announcements</h1>
                         </div>
-                        <div class="bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                            <div class="mb-8">
-                                <h1 class="text-2xl font-semibold">News</h1>
-                                <div class="max-w-lg">
-                                    <p class="text-base text-gray-600">
-                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                        <div class="overflow-y-auto" style="height: 30rem;" x-data="{ modalOpen: false}">
+                            <div class="cursor-pointer border-b-2 p-4 bg-white hover:bg-gray-100" @click="modalOpen = true">
+                                <h1 class="font-semibold text-lg pb-2">
+                                    <span class="flex items-center text-yellow-500">
+                                        <x-heroicon-o-calendar class="h-5 w-5 mr-2"/> 
+                                        27-5-2021
+                                    </span>
+                                </h1>
+                                <p class="text-gray-600 text-base truncate">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                </p>
+                            </div>
+                            <!-- Start modal details -->
+                            <x-general.modal2 modalActive="modalOpen" title="Announcements" modalSize="lg" headerbg="yellow-400" closeBtn="yes">
+                                <x-slot name="icon">
+                                    <x-heroicon-s-information-circle class="w-8 h-8 mr-1" />
+                                </x-slot>
+                                <div class="p-6">
+                                    <h1 class="font-semibold text-lg pb-2">
+                                        <span class="flex items-center text-yellow-500">
+                                            <x-heroicon-o-calendar class="h-5 w-5 mr-2" />
+                                            27-5-2021
+                                        </span>
+                                    </h1>
+                                    <p class="text-gray-600 text-base">Lorem Ipsum is simply dummy text of the printing and typesetting
+                                        industry.
                                         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                                        when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                                     </p>
                                 </div>
+                            </x-general.modal2>
+                            <!-- End modal details -->
+
+                            <div class="cursor-pointer border-b-2 p-4 bg-white hover:bg-gray-100">
+                                <h1 class="font-semibold text-lg pb-2">
+                                    <span class="flex items-center text-yellow-500">
+                                        <x-heroicon-o-calendar class="h-5 w-5 mr-2"/> 
+                                        23-5-2021
+                                    </span>
+                                </h1>
+                                <p class="text-gray-600 text-base truncate">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                </p>
                             </div>
+                            <div class="cursor-pointer border-b-2 p-4 bg-white hover:bg-gray-100">
+                                <h1 class="font-semibold text-lg pb-2">
+                                    <span class="flex items-center text-yellow-500">
+                                        <x-heroicon-o-calendar class="h-5 w-5 mr-2"/> 
+                                        21-5-2021
+                                    </span>
+                                </h1>
+                                <p class="text-gray-600 text-base truncate">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                </p>
+                            </div>
+                            <div class="cursor-pointer border-b-2 p-4 bg-white hover:bg-gray-100">
+                                <h1 class="font-semibold text-lg pb-2">
+                                    <span class="flex items-center text-yellow-500">
+                                        <x-heroicon-o-calendar class="h-5 w-5 mr-2"/> 
+                                        08-5-2021
+                                    </span>
+                                </h1>
+                                <p class="text-gray-600 text-base truncate">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                </p>
+                            </div>
+                            <div class="cursor-pointer border-b-2 p-4 bg-white hover:bg-gray-100">
+                                <h1 class="font-semibold text-lg pb-2">
+                                    <span class="flex items-center text-yellow-500">
+                                        <x-heroicon-o-calendar class="h-5 w-5 mr-2"/> 
+                                        06-5-2021
+                                    </span>
+                                </h1>
+                                <p class="text-gray-600 text-base truncate">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
+                                    when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                                </p>
+                            </div>
+                            
                         </div>
                     </div>
                 </div>
-                <div class="flex col-span-12 lg:col-span-6 xxl:col-span-6 lg:block p-4 bg-white shadow-lg">
+                <div class="flex justify-start col-span-12 lg:col-span-6 xxl:col-span-6 lg:block">
                     <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
                     <script type="text/javascript">
-                        var width= 700;
-                        var height= 550;
-
-                        if ((screen.width<768)) {
-                            width= 250;
-                            height= 250;
-                        }
-                        else if ((screen.width>=768)&&(screen.width<1024)) {
-                            width= 500;
-                            height= 300;
-                        }
-                        else if ((screen.width>=1024)&&(screen.width<1280)) {
-                            width= 500;
-                            height= 300;
-                        }
-                        else if ((screen.width>=1280)) {
-                            width= 500;
-                            height= 300;
-                        }
-
                         new TradingView.widget(
                             {
-                                "width": width,
-                                "height": height,
+                                "autosize": true,
                                 "symbol": "TVC:GOLD",
                                 "timezone": "Asia/Singapore",
-                                "colorTheme": "light",
+                                "theme": "dark",
                                 "style": "1",
                                 "locale": "en",
                                 "toolbar_bg": "#f1f3f6",
@@ -89,7 +140,6 @@
                 </div>
             </div>
         </div>
-
     </div>
 </div> 
 
