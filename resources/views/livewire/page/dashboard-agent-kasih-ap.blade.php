1c0995b11aa72cdddf14f9b54b6a4e7dce25fe30
safwan <safwan@csc.net.my>
2021-06-10 16:19:55 +0800

    edit dashboard header

--- resources/views/livewire/page/dashboard-agent-kasih-ap.blade.php
+++ resources/views/livewire/page/dashboard-agent-kasih-ap.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="z-20 relative mx-auto px-0 my-3">
+    <div class="-mt-52">
         <div class="grid grid-cols-12 gap-6">
             <div class="grid grid-cols-12 col-span-12 gap-6 xxl:col-span-9">
                 <div class="col-span-12 mt-8">
@@ -10,7 +10,7 @@
                         </div>
                     </div>
 
-                    <div class="grid grid-cols-12 gap-6 mt-8">
+                    <div class="grid grid-cols-12 gap-6 mt-10">
                         <x-dashboard.info-card bg="white" title="Pending Approval" value="{{ $pendingApproval->count() }} Users" cardRoute="{{route('pending-approval-kap-agent')}}" >
                             <x-slot name="svg">
                                 <x-heroicon-o-user-group class="text-blue-400 h-7 w-7"/>
@@ -38,13 +38,5 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div class="absolute top-0 left-0 right-2 z-0">
-        <header >
-            <div class="w-full bg-cover bg-center" style="height:13rem; background-image: url({{ asset('img/header.jpg') }});">
-                <div class=" px-8 py-4 h-full w-full bg-opacity-75">
-                </div>
-            </div>
-        </header>
     </div>
 </div>
