994779d7900c8c36cf5ce2084fdd9c3012cb6dfa
safwan <safwan@csc.net.my>
2021-06-15 22:05:20 +0800

    commentout details popup in pending approval (kap)

--- resources/views/livewire/page/kap/pending-approval-kap-agent.blade.php
+++ resources/views/livewire/page/kap/pending-approval-kap-agent.blade.php
@@ -56,13 +56,74 @@
                                 <span class="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-{{ ($lists->profile_c == 1) ? 'green' : 'yellow'}}-100 text-{{ ($lists->profile_c == 1) ? 'green' : 'yellow'}}-800">{{ ($lists->profile_c == 1) ? 'Complete': 'Incomplete'}}</span>
                             </x-table.table-body>
                             <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
-                                @if ($lists->profile_c == 1)
-                                    <button wire:click="approve({{ $lists->id }})"
-                                            class="inline-flex items-center px-4 py-2 font-semibold text-white bg-green-500 rounded-lg hover:bg-green-600">
-                                        <x-heroicon-o-clipboard-check class="w-5 h-5 mr-1" />
-                                        Approve
+                                <div class="flex space-x-2" x-data="{ openModal : false}">
+
+                                    {{-- <button  @click="openModal = true"
+                                        class="inline-flex items-center px-4 py-2 font-semibold text-white bg-indigo-500 rounded-lg hover:bg-indigo-600 focus:outline-none">
+                                        <x-heroicon-o-clipboard-list class="w-5 h-5 mr-1" />
+                                        Details
                                     </button>
-                                @endif
+
+                                    <! -- Start modal -->
+                                    <x-general.new-modal modalName="openModal" size="2xl">
+                                        <div class="flex items-center justify-center w-12 h-12 mx-auto bg-blue-100 rounded-full">
+                                            <x-heroicon-o-identification class="w-6 h-6 text-blue-600" />
+                                        </div>
+                                        <div class="my-3 text-center sm:mt-5">
+                                            <h1 class="text-lg font-bold">Personal Information</h1>
+                                        </div>
+                                        <div class="py-4 px-4">
+                                            <x-form.basic-form>
+                                                <x-slot name="content">
+                                                    <div class="mt-2 leading-4">
+                                                        <div class="grid gap-2 lg:grid-cols-2 sm:grid-cols-1">
+                                                            <x-form.input label="KAP Code" value="" disable="true" />
+                                                            <x-form.input label="Membership ID"  value="" disable="true" />
+
+                                                            <x-form.input label="Name"  value="" />
+                                                            <x-form.input type="email" label="Email Address"  value="" disable="true" />
+
+                                                            <x-form.input label="New IC"  value="" />
+                                                            <x-form.input label="Old IC" value="" />
+                                                            <x-form.input label="Passport / Foreign ID" value="" />
+                                                            <x-form.input label="Police / Army" value="" />
+
+                                                            <x-form.input label="Company No"  value="" />
+
+                                                            <x-form.input label="Phone No" value="" />
+                                                            <x-form.input label="Fax No" value=""/>
+                                                        </div>
+                                                        <div class="grid gap-2 lg:grid-cols-1 sm:grid-cols-1">
+                                                            <x-form.address class="" label="Address" value1="" value2="" value3="" value4="" value5="" value6=""  condition=""/>
+                                                        </div>
+                                                        <div class="mt-5 grid gap-2 lg:grid-cols-2 sm:grid-cols-1">
+                                                            <button type="button" class="inline-flex justify-center w-full px-4 py-2 mt-3 text-base 
+                                                                    font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 
+                                                                    focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:mt-0 sm:col-start-1 
+                                                                    sm:text-sm" @click="openModal = false">
+                                                                    Cancel
+                                                            </button>
+                                                            <a href="#" type="button" class="inline-flex justify-center w-full px-4 py-2 text-base 
+                                                                    font-medium text-white bg-green-600 border border-transparent rounded-md shadow-sm hover:bg-green-700 
+                                                                    focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:col-start-2 sm:text-sm">
+                                                                    Submit
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                </x-slot>
+                                            </x-form.basic-form>
+                                        </div>
+                                    </x-general.new-modal>
+                                    <! -- End modal --> --}}
+                                    
+                                    @if ($lists->profile_c == 1)
+                                        <button wire:click="approve({{ $lists->id }})"
+                                                class="inline-flex items-center px-4 py-2 font-semibold text-white bg-green-500 rounded-lg hover:bg-green-600">
+                                            <x-heroicon-o-clipboard-check class="w-5 h-5 mr-1" />
+                                            Approve
+                                        </button>
+                                    @endif
+                                </div>
                             </x-table.table-body>
                         </tr>
                     @empty
