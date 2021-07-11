3616cecdca24ab669af397d9c23917246bafa27c
Nazirul Fitri Fauzi <nazirulfitrifauzi@gmail.com>
2021-07-12 03:00:14 +0800

    tnc menu

--- app/View/Components/sidebar/NavItem.php
+++ app/View/Components/sidebar/NavItem.php
@@ -9,15 +9,17 @@ class NavItem extends Component
     public $title;
     public $route;
     public $uri;
+    public $target;
 
-    public function __construct($title,$route,$uri)
+    public function __construct($title,$route,$uri,$target="")
     {
         $this->title = $title;
         $this->route = $route;
         $this->uri = $uri;
+        $this->target = $target;
     }
 
-    
+
     public function render()
     {
         return view('components.sidebar.nav-item');
