10c11ef25ad0053da579fd3061d139efaa6bf248
Nazirul Fitri Fauzi <nazirulfitrifauzi@gmail.com>
2021-07-11 18:08:31 +0800

    cashback table detail

--- app/Http/Livewire/Page/Kap/Cashback.php
+++ app/Http/Livewire/Page/Kap/Cashback.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Livewire\Page\Kap;
 
+use App\Models\CommissionDetailKap;
 use Livewire\Component;
 
 class Cashback extends Component
 {
     public function render()
     {
-        return view('livewire.page.kap.cashback');
+        return view('livewire.page.kap.cashback', [
+            'lists' => CommissionDetailKap::paginate(15),
+        ]);
     }
 }
