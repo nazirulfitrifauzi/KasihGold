deec0999a49daa3b63c117e01ab2c4b191bafdac
Nazirul Fitri Fauzi <nazirulfitrifauzi@gmail.com>
2021-07-14 16:20:02 +0800

    group by user id

--- app/Http/Livewire/Page/Kap/Cashback.php
+++ app/Http/Livewire/Page/Kap/Cashback.php
@@ -3,6 +3,7 @@
 namespace App\Http\Livewire\Page\Kap;
 
 use App\Models\CommissionDetailKap;
+use Illuminate\Support\Facades\DB;
 use Livewire\Component;
 
 class Cashback extends Component
@@ -10,7 +11,9 @@ class Cashback extends Component
     public function render()
     {
         return view('livewire.page.kap.cashback', [
-            'lists' => CommissionDetailKap::paginate(15),
+            'lists' => CommissionDetailKap::select("user_id", DB::raw("SUM(commission) as total"))
+                                            ->groupBy("user_id")
+                                            ->get(),
         ]);
     }
 }
