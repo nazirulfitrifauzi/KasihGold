9bc432cfa76c2abd6ee6a4589278089e90852446
Nazirul Fitri Fauzi <nazirulfitrifauzi@gmail.com>
2021-07-29 01:02:37 +0800

    Verify OTP

--- app/Http/Livewire/Auth/VerifyOtp.php
+++ app/Http/Livewire/Auth/VerifyOtp.php
@@ -2,20 +2,77 @@
 
 namespace App\Http\Livewire\Auth;
 
+use App\Models\User;
 use Livewire\Component;
 
 class VerifyOtp extends Component
 {
     public $phone_no;
+    public $first, $second, $third, $fourth, $fifth, $sixth;
+
+    protected $listeners = ['submitData' => 'verifyOTP'];
 
     public function mount()
     {
-        $no = '0189445211';
+        $no = auth()->user()->phone_no;
 
         $x = substr($no, 0, 3);
         $y = str_repeat("X", (strlen($no) - 6));
         $z = substr($no, -3);
         $this->phone_no = '+6 ' . $x.'-'.$y.$z;
+    }
+
+    public function verifyOTP()
+    {
+        $otp = $this->first.$this->second.$this->third.$this->fourth.$this->fifth.$this->sixth;
+
+        // generate OTP code n sms to the user
+        $client     = new \GuzzleHttp\Client();
+        $response = $client->request('POST', "https://api.esms.com.my/sms/otp/verify", ['query' => [
+            'api-key'       => config('esms.key'),
+            'api-secret'    => config('esms.secret'),
+            'phone'         => '6' . auth()->user()->phone_no,
+            'code'          => $otp
+        ]]);
+
+        $content = json_decode($response->getBody(), true);
+
+        if ($content['status'] == 0) {  //success
+            User::whereId(auth()->user()->id)->update([
+                'otp_verified_at' => now(),
+            ]);
+
+            session()->flash('success');
+            session()->flash('title', 'Success!');
+            session()->flash('message', 'Your phone has been verified.');
+
+            return redirect('/profile');
+        } elseif ($content['status'] == 16) {
+            session()->flash('error');
+            session()->flash('title', 'Failed!');
+            session()->flash('message', 'The supplied OTP code is invalid.');
+        } else {
+            session()->flash('warning');
+            session()->flash('title', 'Attention!');
+            session()->flash('message', 'Server error. You may retry again shortly.');
+        }
+    }
+
+    public function resend()
+    {
+        // generate OTP code n sms to the user
+        $client     = new \GuzzleHttp\Client();
+        $client->request('POST', "https://api.esms.com.my/sms/otp/generate", ['query' => [
+            'api-key'       => config('esms.key'),
+            'api-secret'    => config('esms.secret'),
+            'phone'         => '6' . auth()->user()->phone_no,
+            'brand-name'    => 'KASIH AP GOLD',
+            'duration'      => 5,
+        ]]);
+
+        session()->flash('success');
+        session()->flash('title', 'Success!');
+        session()->flash('message', 'New OTP code has been sent to your phone.');
     }
 
     public function render()
--- resources/views/livewire/auth/verify-otp.blade.php
+++ resources/views/livewire/auth/verify-otp.blade.php
@@ -1,30 +1,31 @@
-<div class="min-w-screen h-screen fixed left-0 top-0 flex justify-center items-center inset-0 z-50 outline-none focus:outline-none  overflow-auto"
+<div class="fixed inset-0 top-0 left-0 z-50 flex items-center justify-center h-screen overflow-auto outline-none min-w-screen focus:outline-none"
     style="background: rgba(0, 0, 0, 0.5);">
     <div
-        class="animate__animated animate__bounceInDown w-full  max-w-lg p-5 relative mx-auto my-auto rounded-xl shadow-lg bg-white">
+        class="relative w-full max-w-lg p-5 mx-auto my-auto bg-white shadow-lg animate__animated animate__bounceInDown rounded-xl">
         <!--content-->
         <div class="">
             <!--body-->
-            <div class="text-center p-5 flex-auto justify-center">
+            <div class="justify-center flex-auto p-5 text-center">
                 <x-logo class="w-auto h-16 mx-auto" />
                 <img src="{{ asset('img/otp.png') }}" class="w-auto h-64 mx-auto" />
                     <h1 class="text-2xl font-bold">OTP Verification</h1>
                     <div class="flex flex-col mt-3">
                         <span>Enter the OTP you received at</span> <span class="font-bold">{{ $phone_no }}</span>
                     </div>
-                    <div id="otp" class="flex flex-row justify-center px-2 mt-5 text-center mx-20">
-                        <input class="w-10 h-10 m-2 text-center border rounded form-control" type="text" id="first" maxlength="1" />
-                        <input class="w-10 h-10 m-2 text-center border rounded form-control" type="text" id="second" maxlength="1" /> 
-                        <input class="w-10 h-10 m-2 text-center border rounded form-control" type="text" id="third" maxlength="1" />
-                        <input class="w-10 h-10 m-2 text-center border rounded form-control" type="text" id="fourth" maxlength="1" /> 
-                        <input class="w-10 h-10 m-2 text-center border rounded form-control" type="text" id="fifth" maxlength="1" />
-                        <input class="w-10 h-10 m-2 text-center border rounded form-control" type="text" id="sixth" maxlength="1" />
+                    <div id="otp" class="flex flex-row justify-center px-2 mx-20 mt-5 text-center">
+                        <input class="w-10 h-10 m-2 text-center border rounded form-control" type="text" id="first" maxlength="1" wire:model="first" />
+                        <input class="w-10 h-10 m-2 text-center border rounded form-control" type="text" id="second" maxlength="1" wire:model="second"/>
+                        <input class="w-10 h-10 m-2 text-center border rounded form-control" type="text" id="third" maxlength="1" wire:model="third"/>
+                        <input class="w-10 h-10 m-2 text-center border rounded form-control" type="text" id="fourth" maxlength="1" wire:model="fourth"/>
+                        <input class="w-10 h-10 m-2 text-center border rounded form-control" type="text" id="fifth" maxlength="1" wire:model="fifth"/>
+                        <input class="w-10 h-10 m-2 text-center border rounded form-control" type="text" id="sixth" maxlength="1" wire:model="sixth" onchange="submit()"/>
                     </div>
+                    {{-- {{ $first }}{{ $second }}{{ $third }}{{ $fourth }}{{ $fifth }}{{ $sixth }} --}}
                     <div class="flex justify-center mt-5 text-center">
-                        <a class="flex items-center bg-yellow-400 px-6 py-4 flex justify-center text-white hover:bg-yellow-300 rounded-lg">
-                            <p class="font-bold ml-2">Resend OTP</p>
+                        <button wire:click="resend" class="flex items-center justify-center px-6 py-4 text-white bg-yellow-400 rounded-lg hover:bg-yellow-300">
+                            <p class="ml-2 font-bold">Resend OTP</p>
                             <x-heroicon-s-chevron-right class="w-6 h-6 "/>
-                        </a>
+                        </button>
                     </div>
             </div>
         </div>
@@ -42,9 +43,17 @@
 @endif
 
 <script>
+    $("#sixth").on('input', function(){
+        setTimeout(
+            function() {
+                Livewire.emit('submitData');
+            }, 3000);
+    });
+</script>
+<script>
 document.addEventListener("DOMContentLoaded", function(event) {
     function OTPInput() {
-        const inputs = document.querySelectorAll('#otp > *[id]');
+        // const inputs = document.querySelectorAll('#otp > *[id]');
         for (let i = 0; i < inputs.length; i++) {
             inputs[i].addEventListener('keydown', function(event) {
                 if (event.key==="Backspace" ) {
