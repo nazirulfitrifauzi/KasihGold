b01cca39b085f99ca71899d309ee20af5cb7c6e5
safwan <safwan@csc.net.my>
2021-07-29 23:23:20 +0800

    remove timer otp

--- resources/views/livewire/auth/verify-otp.blade.php
+++ resources/views/livewire/auth/verify-otp.blade.php
@@ -13,34 +13,26 @@
                         <span>Enter the OTP you received at</span> <span class="text-sm font-bold">{{ $phone_no }}</span>
                     </div>
                     <div class="flex flex-row justify-center px-2 mx-20 mt-5 text-center" >
-                        <input class="w-10 h-10 m-2 text-center border rounded form-control inputs" type="text" maxlength="1" wire:model="first" />
-                        <input class="w-10 h-10 m-2 text-center border rounded form-control inputs" type="text" maxlength="1" wire:model="second"/>
-                        <input class="w-10 h-10 m-2 text-center border rounded form-control inputs" type="text" maxlength="1" wire:model="third"/>
-                        <input class="w-10 h-10 m-2 text-center border rounded form-control inputs" type="text" maxlength="1" wire:model="fourth"/>
-                        <input class="w-10 h-10 m-2 text-center border rounded form-control inputs" type="text" maxlength="1" wire:model="fifth"/>
-                        <input class="w-10 h-10 m-2 text-center border rounded form-control inputs" type="text" maxlength="1" wire:model="sixth" />
+                        <input class="w-10 h-10 m-2 text-center border rounded form-control inputs" type="text" maxlength="1" wire:model.silent="first"  />
+                        <input class="w-10 h-10 m-2 text-center border rounded form-control inputs" type="text" maxlength="1" wire:model.silent="second"/>
+                        <input class="w-10 h-10 m-2 text-center border rounded form-control inputs" type="text" maxlength="1" wire:model.silent="third"/>
+                        <input class="w-10 h-10 m-2 text-center border rounded form-control inputs" type="text" maxlength="1" wire:model.silent="fourth"/>
+                        <input class="w-10 h-10 m-2 text-center border rounded form-control inputs" type="text" maxlength="1" wire:model.silent="fifth"/>
+                        <input class="w-10 h-10 m-2 text-center border rounded form-control inputs" type="text" maxlength="1" wire:model.silent="sixth" />
                         <input class="w-10 h-10 m-2 text-center border rounded form-control inputs" type="hidden" maxlength="1" />
                     </div>
-
-                    <div class="flex justify-center mt-3 text-center">
+                    <div class="mt-5">
+                        <p class="text-gray-700 text-sm">didn't you receive the OTP? 
+                            <span>
+                                <button  wire:click="resend" class="text-yellow-400 hover:text-yellow-300 text-lg font-semibold ml-1"> Resend</button>
+                            </span>
+                        </p>
+                    </div>
+                    <div class="border-t-2 my-2"></div>
+                    <div class="flex justify-center mt-5 text-center space-x-2">
                         <div>
                             <button wire:click="verifyOTP" class="flex items-center justify-center px-6 py-4 text-white bg-yellow-400 rounded-lg hover:bg-yellow-300">
                                 <p class="ml-2 font-bold">Verify OTP</p>
-                                <x-heroicon-s-chevron-right class="w-6 h-6 "/>
-                            </button>
-                        </div>
-                    </div>
-
-                    <div class="flex justify-center mt-2 text-center">
-                        <div id="timerBtn">
-                            <div class="p-4 text-red-600 border-2 border-red-400" id="time">
-                                05:00
-                            </div>
-                        </div>
-
-                        <div class="cursor-pointer" id="otpBtn" style="display:none;" >
-                            <button wire:click="resend" onchange="runtimer()" class="flex items-center justify-center px-6 py-4 text-white bg-gray-800 rounded-lg hover:bg-gray-700">
-                                <p class="ml-2 font-bold">Resend OTP</p>
                                 <x-heroicon-s-chevron-right class="w-6 h-6 "/>
                             </button>
                         </div>
@@ -72,49 +64,4 @@
             $(this).prev('.inputs').focus();
         }
     });
-</script>
-
-<script>
-    function runtimer() {
-        var timerBtn = document.getElementById("timerBtn");
-        var otpBtn = document.getElementById("otpBtn");
-        timerBtn.style.display = "none";
-        otpBtn.style.display = "block";
-
-        var fiveMinutes = 1 * 5,
-        // var fiveMinutes = 60 * 5,
-        display = document.querySelector('#time');
-        startTimer(fiveMinutes, display);
-    }
-
-    function startTimer(duration, display) {
-        var timer = duration, minutes, seconds;
-        setInterval(function () {
-            minutes = parseInt(timer / 60, 10);
-            seconds = parseInt(timer % 60, 10);
-            minutes = minutes < 10 ? "0" + minutes : minutes;
-            seconds = seconds < 10 ? "0" + seconds : seconds;
-            display.textContent = minutes + ":" + seconds;
-            if (--timer < 0) {
-                timer = duration;
-                otpBtn.style.display = "block";
-                timerBtn.style.display = "none";
-            }
-        }, 1000);
-    }
-
-    window.onload = function () {
-        @if (session('error'))
-        otpBtn.style.display = 'block';
-        timerBtn.style.display = "none";
-        @else
-        otpBtn.style.display = 'none';
-        timerBtn.style.display = "block";
-        @endif
-
-        // var fiveMinutes = 60 * 5,
-        var fiveMinutes = 1 * 5,
-            display = document.querySelector('#time');
-        startTimer(fiveMinutes, display);
-    };
 </script>
