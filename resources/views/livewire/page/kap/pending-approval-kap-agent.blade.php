88e78c5713e4cfaab39d77f610d933f932f00d84
Nazirul Fitri Fauzi <nazirulfitrifauzi@gmail.com>
2021-08-01 13:48:02 +0800

    fixed indexing

--- resources/views/livewire/page/kap/pending-approval-kap-agent.blade.php
+++ resources/views/livewire/page/kap/pending-approval-kap-agent.blade.php
@@ -42,10 +42,11 @@
                     <x-table.table-header class="text-left" value="Action" sort="" />
                 </x-slot>
                 <x-slot name="tbody">
+                    @php $i = 1 + $list->getCurrentPage() * $list->getPerPage() - $list->getPerPage(); @endphp
                     @forelse ($list as $index => $lists)
                         <tr wire:key="lists-{{ $lists->id }}">
                             <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
-                                <p>{{ $loop->iteration  }}</p>
+                                <p>{{ $i }}</p>
                             </x-table.table-body>
                             <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
                                 <p>{{ $lists->name }}</p>
