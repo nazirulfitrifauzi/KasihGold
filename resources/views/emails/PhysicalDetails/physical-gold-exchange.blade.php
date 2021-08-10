fa7adae92c4c45b677152be6f0eb8d7f03d493df
safwan <safwan@csc.net.my>
2021-08-10 17:36:13 +0800

    add email physical gold exchange

---
+++ resources/views/emails/PhysicalDetails/physical-gold-exchange.blade.php
@@ -0,0 +1,492 @@
+<!DOCTYPE html
+    PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="color-scheme" content="light">
+    <meta name="supported-color-schemes" content="light">
+    <style>
+        @media only screen and (max-width: 600px) {
+            .inner-body {
+                width: 100% !important;
+            }
+
+            .footer {
+                width: 100% !important;
+            }
+        }
+
+        @media only screen and (max-width: 500px) {
+            .button {
+                width: 100% !important;
+            }
+        }
+
+        /* Base */
+
+        body,
+        body *:not(html):not(style):not(br):not(tr):not(code) {
+            box-sizing: border-box;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif,
+                'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol';
+            position: relative;
+        }
+
+        body {
+            -webkit-text-size-adjust: none;
+            background-color: #ffffff;
+            color: #3a3a3a;
+            height: 100%;
+            line-height: 1.4;
+            margin: 0;
+            padding: 0;
+            width: 100% !important;
+        }
+
+        p,
+        ul,
+        ol,
+        blockquote {
+            line-height: 1.4;
+            text-align: left;
+        }
+
+        a {
+            color: #3869d4;
+        }
+
+        a img {
+            border: none;
+        }
+
+        /* Typography */
+
+        h1 {
+            color: #3d4852;
+            font-size: 18px;
+            font-weight: bold;
+            margin-top: 0;
+            text-align: left;
+        }
+
+        h2 {
+            font-size: 16px;
+            font-weight: bold;
+            margin-top: 0;
+            text-align: left;
+        }
+
+        h3 {
+            font-size: 14px;
+            font-weight: bold;
+            margin-top: 0;
+            text-align: left;
+        }
+
+        p {
+            font-size: 16px;
+            line-height: 1.2em;
+            margin-top: 0;
+            text-align: center;
+        }
+
+        p.sub {
+            font-size: 12px;
+        }
+
+        img {
+            max-width: 100%;
+        }
+
+        /* Layout */
+
+        .wrapper {
+            -premailer-cellpadding: 0;
+            -premailer-cellspacing: 0;
+            -premailer-width: 100%;
+            background-color: #2c2b2b;
+            margin: 0;
+            padding: 0;
+            width: 100%;
+        }
+
+        .content {
+            -premailer-cellpadding: 0;
+            -premailer-cellspacing: 0;
+            -premailer-width: 100%;
+            margin: 0;
+            padding: 0;
+            width: 100%;
+        }
+
+        /* Header */
+
+        .header {
+            padding: 25px 0;
+            text-align: center;
+        }
+
+        .header1 {
+            padding: 5px 0;
+            text-align: center;
+        }
+
+        .header a {
+            color: #3d4852;
+            font-size: 19px;
+            font-weight: bold;
+            text-decoration: none;
+        }
+
+        /* Logo */
+
+        .logo {
+            height: 75px;
+            max-height: 75px;
+            width: 75px;
+        }
+
+        /* Body */
+
+        .body {
+            -premailer-cellpadding: 0;
+            -premailer-cellspacing: 0;
+            -premailer-width: 100%;
+            background-color: #2c2b2b;
+            border-bottom: 1px solid #2c2b2b;
+            border-top: 1px solid #2c2b2b;
+            margin: 0;
+            padding: 0;
+            width: 100%;
+        }
+
+        .inner-body {
+            -premailer-cellpadding: 0;
+            -premailer-cellspacing: 0;
+            -premailer-width: 570px;
+            background-color: #ffffff;
+            border-color: #e8e5ef;
+            border-radius: 20px;
+            border-width: 1px;
+            margin: 0 auto;
+            padding: 0;
+            width: 570px;
+            border-top: 5px solid rgb(255, 208, 0);
+        }
+
+        /* Subcopy */
+
+        .subcopy {
+            border-top: 1px solid #e8e5ef;
+            margin-top: 25px;
+            padding-top: 25px;
+        }
+
+        .subcopy p {
+            font-size: 14px;
+        }
+
+        /* Footer */
+
+        .footer {
+            -premailer-cellpadding: 0;
+            -premailer-cellspacing: 0;
+            -premailer-width: 570px;
+            margin: 0 auto;
+            padding: 0;
+            text-align: center;
+            width: 570px;
+        }
+
+        .footer p {
+            color: #b0adc5;
+            font-size: 12px;
+            text-align: center;
+        }
+
+        .footer a {
+            color: #b0adc5;
+            text-decoration: underline;
+        }
+
+        /* Tables */
+
+        .table table {
+            -premailer-cellpadding: 0;
+            -premailer-cellspacing: 0;
+            -premailer-width: 100%;
+            margin: 30px auto;
+            width: 100%;
+        }
+
+        .table th {
+            border-bottom: 1px solid #edeff2;
+            margin: 0;
+            padding-bottom: 8px;
+        }
+
+        .table td {
+            color: #74787e;
+            font-size: 15px;
+            line-height: 18px;
+            margin: 0;
+            padding: 10px 0;
+        }
+
+        .content-cell {
+            max-width: 100vw;
+            padding: 32px;
+            text-align: center;
+        }
+
+        .content-cell4 {
+            max-width: 100vw;
+            padding: 10px;
+            text-align: center;
+        }
+
+        /* Buttons */
+
+        .action {
+            -premailer-cellpadding: 0;
+            -premailer-cellspacing: 0;
+            -premailer-width: 100%;
+            margin: 30px auto;
+            padding: 0;
+            text-align: center;
+            width: 100%;
+        }
+
+        .button {
+            -webkit-text-size-adjust: none;
+            border-radius: 4px;
+            color: #fff;
+            display: inline-block;
+            overflow: hidden;
+            text-decoration: none;
+        }
+
+        .button-blue,
+        .button-primary {
+            background-color: #2d3748;
+            border-bottom: 8px solid #2d3748;
+            border-left: 18px solid #2d3748;
+            border-right: 18px solid #2d3748;
+            border-top: 8px solid #2d3748;
+        }
+
+        .button-green,
+        .button-success {
+            background-color: #48bb78;
+            border-bottom: 8px solid #48bb78;
+            border-left: 18px solid #48bb78;
+            border-right: 18px solid #48bb78;
+            border-top: 8px solid #48bb78;
+        }
+
+        .button-red,
+        .button-error {
+            background-color: #e53e3e;
+            border-bottom: 8px solid #e53e3e;
+            border-left: 18px solid #e53e3e;
+            border-right: 18px solid #e53e3e;
+            border-top: 8px solid #e53e3e;
+        }
+
+        .button-yellow,
+        .button-warning {
+            background-color: #fcc200;
+            border-bottom: 8px solid #fcc200;
+            border-left: 18px solid #fcc200;
+            border-right: 18px solid #fcc200;
+            border-top: 8px solid #fcc200;
+        }
+
+        /* Panels */
+
+        .panel {
+            border-left: #2d3748 solid 4px;
+            margin: 21px 0;
+        }
+
+        .panel-content {
+            background-color: #edf2f7;
+            color: #718096;
+            padding: 16px;
+        }
+
+        .panel-content p {
+            color: #718096;
+        }
+
+        .panel-item {
+            padding: 0;
+        }
+
+        .panel-item p:last-of-type {
+            margin-bottom: 0;
+            padding-bottom: 0;
+        }
+
+        /* Utilities */
+
+        .break-all {
+            word-break: break-all;
+        }
+
+    </style>
+</head>
+
+<body>
+
+    <table class="wrapper" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+        <tr>
+            <td align="center">
+                <table class="" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+                    <!-- Email Body -->
+                    <tr>
+                        <td class="header"></td>
+                    </tr>
+                    <tr>
+                        <td class="body" width="100%" cellpadding="0" cellspacing="0">
+                            <table class="inner-body" align="center" width="570" cellpadding="0" cellspacing="0"
+                                role="presentation">
+                                <!-- Body content -->
+                                <tr>
+                                    <td class="header">
+                                        <a href="#" style="display: inline-block;">
+                                            <img src="{{ asset('img/kasihAPGold.png') }}" style="width:150px; heigth:150px; "/>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="header1">
+                                        <a href="#" style="display: inline-block;">
+                                            <img src="{{ asset('img/notify.png') }}" style="width:150px; heigth:150px; "/>
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td class="" style="padding-top: 4px !important;">
+                                    <p style="font-weight: bold; font-size:20px;">A new user has requested a physical gold exchange</p>
+                                    </td>
+                                </tr>
+                                <tr>
+                                <td>
+                                    <table align="center" style="margin-bottom:20px; width:80%">
+                                            <tr style="background-color: #23c503">
+                                                <th colspan="2"  style="padding: 10px; font-weight: bold; font-size:14px; text-align:center; color:white">Physical Conversion-Details</th>                                             
+                                            </tr>
+                                            <tr>
+                                                <td style="padding: 10px;"></td>
+                                            </tr>
+                                            <tr>
+                                                <td style="padding-left: 10px; padding-right: 10px;">
+                                                    <p style="font-weight: bold; font-size:12px; text-align:left;">Transaction Number :</p>
+                                                </td>
+                                                <td style="padding-left: 10px; padding-right: 10px;">
+                                                    <p style="font-weight: bold; font-size:12px; text-align:right;"> Vxs2eUot</p>
+                                                </td>
+                                                
+                                            </tr>
+                                            <tr>
+                                                <td style="padding-left: 10px; padding-right: 10px;">
+                                                    <p style="font-weight: bold; font-size:12px; text-align:left;">Transaction date :</p>
+                                                </td>
+                                                <td style="padding-left: 10px; padding-right: 10px;">
+                                                    <p style="font-weight: bold; font-size:12px; text-align:right;"> 10/8/2021</p>
+                                                </td>
+                                                
+                                            </tr>
+                                            <tr>
+                                                <td style="padding-left: 10px; padding-right: 10px;">
+                                                    <p style="font-weight: bold; font-size:12px; text-align:left;">Customer Name :</p>
+                                                </td>
+                                                <td style="padding-left: 10px; padding-right: 10px;">
+                                                    <p style="font-weight: bold; font-size:12px; text-align:right;"> Muhammad Safwan</p>
+                                                </td>
+                                                
+                                            </tr>
+                                            <tr>
+                                                <td style="padding-left: 10px; padding-right: 10px;">
+                                                    <p style="font-weight: bold; font-size:12px; text-align:left;">Customer ID :</p>
+                                                </td>
+                                                <td style="padding-left: 10px; padding-right: 10px;">
+                                                    <p style="font-weight: bold; font-size:12px; text-align:right;"> 1337</p>
+                                                </td>
+                                                
+                                            </tr>
+                                            <tr>
+                                                <td style="padding-left: 10px; padding-right: 10px;">
+                                                    <p style="font-weight: bold; font-size:12px; text-align:left;">Address : </p>
+                                                </td>
+                                                <td style="padding-left: 10px; padding-right: 10px;">
+                                                    <p style="font-weight: bold; font-size:12px; text-align:right;"> Lot kampung 43000 kajang selangor</p>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td style="padding-left: 10px; padding-right: 10px;">
+                                                    <p style="font-weight: bold; font-size:12px; text-align:left;">Phone Number : </p>
+                                                </td>
+                                                <td style="padding-left: 10px; padding-right: 10px;">
+                                                    <p style="font-weight: bold; font-size:12px; text-align:right;"> 012223456</p>
+                                                </td>                                             
+                                            </tr>
+
+                                            <tr>
+                                                <td colspan="2" style="padding-bottom:10px; border-top: 1px solid rgb(196, 196, 196);"></td>                                             
+                                            </tr>
+
+                                            <tr>
+                                                <td style="padding-left: 10px; padding-right: 10px;">
+                                                    <p style="font-weight: bold; font-size:16px; text-align:left;">Total Gold</p>
+                                                    <p style="font-weight: bold; font-size:12px; text-align:left;">1 Gram</p>
+                                                    <p style="font-weight: bold; font-size:12px; text-align:left;">0.25 Gram</p>
+                                                </td>
+                                                <td style="padding-left: 10px; padding-right: 10px;">
+                                                    <p style="font-weight: bold; font-size:16px; text-align:right;"> 1.75 G</p>
+                                                    <p style="font-weight: bold; font-size:12px; text-align:right;">1 pcs</p>
+                                                    <p style="font-weight: bold; font-size:12px; text-align:right;">3 pcs</p>
+                                                </td>                                             
+                                            </tr>
+
+                                            <tr>
+                                            <td colspan="2" style="padding-bottom:10px; border-top: 1px solid rgb(196, 196, 196);"></td>                                             
+                                            </tr>
+
+                                            <tr>
+                                                <td colspan="2" style="font-weight: bold; font-size:12px; text-align:center;">thank you for using Kasih AP!</td>                                             
+                                            </tr>
+                                            <tr>
+                                                <td colspan="2" style="padding-top:5px; font-weight: bold; font-size:12px; text-align:center;">if you have any questions please contact 
+                                                    <a href="mailto:customersupport@kasihapgold.com" style="color:#fcc200">Kasih AP Customer Support</a>
+                                                </td>                                             
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                            
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0"
+                                role="presentation">
+                                <tr>
+                                    <td class="content-cell" align="center" style="color:rgba(253, 194, 2, 0.979)">
+                                        © {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
