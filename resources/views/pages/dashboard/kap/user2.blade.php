5423782d0dc6a42b5287666eedfcc6d1b69d3588
safwan <safwan@csc.net.my>
2021-08-10 19:51:47 +0800

    uncomment news (dashboard)

--- resources/views/pages/dashboard/kap/user2.blade.php
+++ resources/views/pages/dashboard/kap/user2.blade.php
@@ -1,14 +1,21 @@
 <div class="col-span-12 sm:col-span-12 md:col-span-12 lg:col-span-4 xxl:col-span-4">
     <x-stacked.stacked-list title="Announcements" class="mb-6">
-        {{-- <li class="px-2 py-5 bg-white hover:bg-gray-100"  x-data="{ modalOpen: false}">
+        <li class="px-2 py-5 bg-white hover:bg-gray-100"  x-data="{ modalOpen: false}">
             <div class="relative focus-within:ring-2 focus-within:ring-indigo-500" @click="modalOpen = true">
-                <h3 class="text-sm font-semibold text-gray-800" >
-                    <a href="#" class="hover:underline focus:outline-none">
-                        <span class="absolute inset-0" aria-hidden="true"></span>
-                        Office closed on July 2nd
-                    </a>
-                </h3>
-                <p class="mt-1 text-sm text-gray-600 truncate line-clamp-2">
+                <div class="flex justify-between items-center">
+                    <div>
+                        <h3 class="text-sm font-semibold text-gray-800" >
+                            <a href="#" class="hover:underline focus:outline-none">
+                                <span class="absolute inset-0" aria-hidden="true"></span>
+                                Office closed on July 2nd
+                            </a>
+                        </h3>
+                    </div>
+                    <div>
+                        <span class="inline-flex items-center px-3 py-0.5 rounded-full text-xs font-medium bg-red-100 text-red-800">Incomplete</span>
+                    </div>
+                </div>
+                <p class="mt-3 text-sm text-gray-600 truncate line-clamp-2">
                     Cum qui rem deleniti. Suscipit in dolor veritatis sequi aut. Vero ut
                     earum quis deleniti. Ut a sunt eum cum ut repudiandae possimus.
                     Nihil ex tempora neque cum consectetur dolores.
@@ -34,15 +41,15 @@
                 </div>
             </x-general.modal2>
             <!-- End modal details -->
-        </li> --}}
+        </li>
         
         <x-slot name="button">
-            {{-- <div class="mt-6">
+            <div class="mt-6">
                 <a href="{{route('all-news')}}"
                     class="flex items-center justify-center w-full px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none hover:bg-blue-500 hover:text-white">
                     View all
                 </a>
-            </div> --}}
+            </div>
         </x-slot>
     </x-stacked.stacked-list>
 </div>
