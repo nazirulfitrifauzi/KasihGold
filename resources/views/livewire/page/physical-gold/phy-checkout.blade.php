56bc8e9278a3fac4c9c9fd96cd96dc344b2c4ba0
safwan <safwan@csc.net.my>
2021-08-11 23:16:02 +0800

    mobile view for Physical Gold Checkout

--- resources/views/livewire/page/physical-gold/phy-checkout.blade.php
+++ resources/views/livewire/page/physical-gold/phy-checkout.blade.php
@@ -26,63 +26,114 @@
                                 </div>
                         </x-general.price-card>
                     </x-general.grid>
-                    <x-table.table>
-                        <x-slot name="thead">
-                            <x-table.table-header class="text-left" value="Product" sort="" />
-                            <x-table.table-header class="text-left" value="Grammage" sort="" />
-                            <x-table.table-header class="text-left" value="Quantity" sort="" />
-                            <x-table.table-header class="text-left" value="Total Grammage" sort="" />
-                        </x-slot>
-                        <x-slot name="tbody">
 
+                    <! -- Start desktop view -->
+                    <div class="hidden lg:block">
+                        <x-table.table>
+                            <x-slot name="thead">
+                                <x-table.table-header class="text-left" value="Product" sort="" />
+                                <x-table.table-header class="text-left" value="Grammage" sort="" />
+                                <x-table.table-header class="text-left" value="Quantity" sort="" />
+                                <x-table.table-header class="text-left" value="Total Grammage" sort="" />
+                            </x-slot>
+                            <x-slot name="tbody">
+
+                                @foreach ($gtype as $types)
+                                <tr>
+                                    <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
+                                        <div class="flex items-center space-x-3">
+                                            <img class="object-cover w-16 h-16 rounded" src="{{ asset('img/gold/'.$types->prod_img1) }}" alt="">
+                                            <div>
+                                                <h3 class="text-sm font-semibold">{{$types->prod_name}}</h3>
+                                            </div>
+                                        </div>
+                                    </x-table.table-body>
+
+                                    <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
+                                        <p>{{number_format($types->prod_weight,2)}} Gram</p>
+                                    </x-table.table-body>
+
+                                    <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
+                                        <div class="relative flex flex-row w-24 h-10 mt-1 bg-transparent rounded-lg">
+                                            <button  wire:click="exitProd({{-1}},'{{$types->prod_weight}}')"
+                                                class="w-20 h-full text-gray-600 bg-gray-300 rounded-l cursor-pointer hover:text-gray-700 hover:bg-gray-400 focus:outline-none">
+                                                <span class="m-auto text-2xl font-thin">−</span>
+                                            </button>
+                                            <input type="text"
+                                                class="flex items-center justify-center w-full font-semibold text-center text-gray-700 bg-gray-300 outline-none focus:outline-none text-md hover:text-black focus:text-black md:text-basecursor-default"
+                                                name="custom-input-number" wire:model="goldbar{{$types->prod_cat}}" value="goldbar{{$types->prod_cat}}" ></input>
+                                            <button  wire:click="exitProd({{1}},'{{$types->prod_weight}}')"
+                                                class="w-20 h-full text-gray-600 bg-gray-300 rounded-r cursor-pointer hover:text-gray-700 hover:bg-gray-400 focus:outline-none">
+                                                <span class="m-auto text-2xl font-thin">+</span>
+                                            </button>
+                                        </div>
+                                    </x-table.table-body>
+
+                                    <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
+                                        @if($types->prod_cat=="063")
+                                        <p>{{number_format($this->goldbar063*0.25,2)}} Gram</p>
+                                        @elseif($types->prod_cat=="064")
+                                        <p>{{number_format($this->goldbar064,2)}} Gram</p>
+                                        @endif
+                                    </x-table.table-body>
+
+                                </tr>
+                                @endforeach
+
+
+                            </x-slot>
+                            <div class="px-2 py-2">
+                                {{-- {{ $list->links('pagination::tailwind') }} --}}
+                            </div>
+                        </x-table.table>
+                    </div>
+                    <! -- End desktop view -->
+
+                    <!--Start Mobile view-->
+                    <div class="block lg:hidden">
+                        <div class="p-4 border-2 rounded-md">
                             @foreach ($gtype as $types)
-                            <tr>
-                                <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
-                                    <div class="flex items-center space-x-3">
+                            <div class="py-2 border-b-2">
+                                <div class="flex items-center justify-between">
+                                    <div>
                                         <img class="object-cover w-16 h-16 rounded" src="{{ asset('img/gold/'.$types->prod_img1) }}" alt="">
-                                        <div>
-                                            <h3 class="text-sm font-semibold">{{$types->prod_name}}</h3>
+                                        <h3 class="text-sm font-semibold">{{$types->prod_name}}</h3>
+                                    </div>
+                                    <div>                                                                                     
+                                        <div class="relative flex flex-row w-24 h-10 mt-1 bg-transparent rounded-lg">
+                                            <button  wire:click="exitProd({{-1}},'{{$types->prod_weight}}')"
+                                                class="w-20 h-full text-gray-600 bg-gray-300 rounded-l cursor-pointer hover:text-gray-700 hover:bg-gray-400 focus:outline-none">
+                                                <span class="m-auto text-2xl font-thin">−</span>
+                                            </button>
+                                            <input type="text"
+                                                class="flex items-center justify-center w-full font-semibold text-center text-gray-700 bg-gray-300 outline-none focus:outline-none text-md hover:text-black focus:text-black md:text-basecursor-default"
+                                                name="custom-input-number" wire:model="goldbar{{$types->prod_cat}}" value="goldbar{{$types->prod_cat}}" ></input>
+                                            <button  wire:click="exitProd({{1}},'{{$types->prod_weight}}')"
+                                                class="w-20 h-full text-gray-600 bg-gray-300 rounded-r cursor-pointer hover:text-gray-700 hover:bg-gray-400 focus:outline-none">
+                                                <span class="m-auto text-2xl font-thin">+</span>
+                                            </button>
                                         </div>
                                     </div>
-                                </x-table.table-body>
-
-                                <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
-                                    <p>{{number_format($types->prod_weight,2)}} Gram</p>
-                                </x-table.table-body>
-
-                                <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
-                                    <div class="relative flex flex-row w-24 h-10 mt-1 bg-transparent rounded-lg">
-                                        <button  wire:click="exitProd({{-1}},'{{$types->prod_weight}}')"
-                                            class="w-20 h-full text-gray-600 bg-gray-300 rounded-l cursor-pointer hover:text-gray-700 hover:bg-gray-400 focus:outline-none">
-                                            <span class="m-auto text-2xl font-thin">−</span>
-                                        </button>
-                                        <input type="text"
-                                            class="flex items-center justify-center w-full font-semibold text-center text-gray-700 bg-gray-300 outline-none focus:outline-none text-md hover:text-black focus:text-black md:text-basecursor-default"
-                                            name="custom-input-number" wire:model="goldbar{{$types->prod_cat}}" value="goldbar{{$types->prod_cat}}" ></input>
-                                        <button  wire:click="exitProd({{1}},'{{$types->prod_weight}}')"
-                                            class="w-20 h-full text-gray-600 bg-gray-300 rounded-r cursor-pointer hover:text-gray-700 hover:bg-gray-400 focus:outline-none">
-                                            <span class="m-auto text-2xl font-thin">+</span>
-                                        </button>
+                                </div>
+                                <div class="flex justify-between mt-1">
+                                    <div>
+                                        <p class="text-xs text-gray-500">GRAMMAGE</p>
+                                        <p class='text-sm font-semibold'>{{number_format($types->prod_weight,2)}} Gram</p>
                                     </div>
-                                </x-table.table-body>
-
-                                <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
-                                    @if($types->prod_cat=="063")
-                                    <p>{{number_format($this->goldbar063*0.25,2)}} Gram</p>
-                                    @elseif($types->prod_cat=="064")
-                                    <p>{{number_format($this->goldbar064,2)}} Gram</p>
-                                    @endif
-                                </x-table.table-body>
-
-                            </tr>
+                                    <div>
+                                        <p class="text-xs text-gray-500">TOTAL GRAMMAGE</p>
+                                        @if($types->prod_cat=="063")
+                                            <p class='text-sm font-semibold text-right'>{{number_format($this->goldbar063*0.25,2)}} Gram</p>
+                                        @elseif($types->prod_cat=="064")
+                                            <p class='text-sm font-semibold text-right'>{{number_format($this->goldbar064,2)}} Gram</p>
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
                             @endforeach
-
-
-                        </x-slot>
-                        <div class="px-2 py-2">
-                            {{-- {{ $list->links('pagination::tailwind') }} --}}
                         </div>
-                    </x-table.table>
+                    </div>
+                    <!--End Mobile view-->
 
 
                     <! -- Start Checkout -->
