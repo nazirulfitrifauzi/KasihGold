e980530813f3d4b4021f38ff17a5a61bf8392fa2
safwan <safwan@csc.net.my>
2021-08-13 23:40:41 +0800

    mobile table

--- resources/views/components/table/table.blade.php
+++ resources/views/components/table/table.blade.php
@@ -1,3 +1,48 @@
+<style>
+    @media
+    only screen 
+    and (max-width: 760px), (min-device-width: 768px) 
+    and (max-device-width: 1024px)  {
+
+		/* Force table to not be like tables anymore */
+		table, thead, tbody, th, td, tr {
+			display: block;
+		}
+
+		/* Hide table headers (but not display: none;, for accessibility) */
+		thead tr {
+			position: absolute;
+			top: -9999px;
+			left: -9999px;
+		}
+
+    tr {
+        margin: 0 0 2rem 0;
+        border: 1px solid #ddd;
+    }
+    
+
+    td {
+        /* Behave  like a "row" */
+        border: none;
+        border-bottom: 1px solid #eee;
+        position: relative;
+        padding-left: 50%;
+    }
+
+    td:before {
+        /* Now like a table header */
+        position: absolute;
+        /* Top/left values mimic padding */
+        top: 0;
+        left: 6px;
+        width: 45%;
+        padding-right: 10px;
+        white-space: nowrap;
+    }
+
+	}
+</style>
 <div class="flex flex-col">
     <div class="-my-2 overflow-x-auto">
         <div class="py-2 align-middle inline-block min-w-full">
@@ -21,3 +66,4 @@
         </div>
     </div>
 </div>
+
--- resources/views/livewire/page/downline/downline-detail.blade.php
+++ resources/views/livewire/page/downline/downline-detail.blade.php
@@ -14,82 +14,105 @@
                 <x-toaster.warning title="{{ session('title') }}" message="{{ session('message') }}"/>
             @endif
         </div>
-
-        <div class="p-4 mt-8 mb-20 bg-white sm:mb-0">
-            <div class="flex justify-between my-4">
-                {{-- <div wire:loading>
-                    <div class="absolute flex items-center justify-center p-4 text-white bg-yellow-400 rounded"
-                        style="left: 50%; top:50%">
-                        <x-heroicon-o-cog class="-ml-0.5 mr-2 h-8 w-8 animate-spin" />
-                        <p class="text-sm">Waiting<span class="animate-ping">...</span></p>
-                    </div>
-                </div> --}}
-                {{-- <div class="flex items-center">
-                    <x-form.search-input />
-                </div> --}}
-            </div>
-            <x-table.table>
-                <x-slot name="thead">
-                    <x-table.table-header class="text-left" value="No" sort="" />
-                    <x-table.table-header class="text-left" value="Name" sort="" />
-                    <x-table.table-header class="text-left" value="Email" sort="" />
-                    <x-table.table-header class="text-left" value="Contact No." sort="" />
-                    @if (auth()->user()->role != 1)
-                        <x-table.table-header class="text-left" value="Membership ID" sort="" />
-                    @elseif(auth()->user()->role == 1)
-                        <x-table.table-header class="text-left" value="Referral Code" sort="" />
-                    @endif
-                </x-slot>
-                <x-slot name="tbody">
-                    @forelse ($activeUser as $index => $lists)
-                        <tr wire:key="lists-{{ $lists->id }}">
-                            <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
-                                <p>{{ $loop->iteration  }}</p>
-                            </x-table.table-body>
-                            <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
-                                <p>{{ $lists->user->name }}</p>
-                            </x-table.table-body>
-                            <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
-                                <p>{{ $lists->user->email }}</p>
-                            </x-table.table-body>
-                            <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
-                                @if($lists->user->role == 3)
-                                    <p>{{ $lists->user->profile->phone1 }}</p>
-                                @elseif ($lists->user->role == 4)
-                                    <p>{{ $lists->user->phone_no }}</p>
+        <div class="col-span-12 lg:col-span-12 xxl:col-span-12 mb-20 sm:mb-0">
+            <div class="grid grid-cols-12 gap-6">
+                <div class="col-span-12 lg:col-span-12 xxl:col-span-12">
+                    <div class="p-4 mt-8 mb-20 bg-white sm:mb-0">
+                        <div class="flex justify-between my-4">
+                            {{-- <div wire:loading>
+                                <div class="absolute flex items-center justify-center p-4 text-white bg-yellow-400 rounded"
+                                    style="left: 50%; top:50%">
+                                    <x-heroicon-o-cog class="-ml-0.5 mr-2 h-8 w-8 animate-spin" />
+                                    <p class="text-sm">Waiting<span class="animate-ping">...</span></p>
+                                </div>
+                            </div> --}}
+                            {{-- <div class="flex items-center">
+                                <x-form.search-input />
+                            </div> --}}
+                        </div>
+                        <x-table.table>
+                            <x-slot name="thead">
+                                <x-table.table-header class="text-left" value="No" sort="" />
+                                <x-table.table-header class="text-left" value="Name" sort="" />
+                                <x-table.table-header class="text-left" value="Email" sort="" />
+                                <x-table.table-header class="text-left" value="Contact No." sort="" />
+                                @if (auth()->user()->role != 1)
+                                    <x-table.table-header class="text-left" value="Membership ID" sort="" />
+                                @elseif(auth()->user()->role == 1)
+                                    <x-table.table-header class="text-left" value="Referral Code" sort="" />
                                 @endif
-                            </x-table.table-body>
-                            @if (auth()->user()->role != 1)
-                                <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
-                                    <p>{{ $lists->user->profile->membership_id }}</p>
-                                </x-table.table-body>
-                            @elseif(auth()->user()->role == 1)
-                                <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
-                                    @if ($lists->user->referralCode == NULL)
-                                        <div class="flex">
-                                            <button wire:click="generate({{ $lists->user->id }})" class="flex items-center justify-center px-2 py-2 text-white bg-green-400 rounded-lg hover:bg-green-300">
-                                                <x-heroicon-o-key class="w-6 h-6 "/>
-                                                <p class="ml-2 font-bold">Generate</p>
-                                            </button>
-                                        </div>
-                                    @else
-                                        <p>{{ $lists->user->referralCode->referral_code }}</p>
-                                    @endif
-                                </x-table.table-body>
-                            @endif
-                        </tr>
-                    @empty
-                        <tr>
-                            <x-table.table-body colspan="4" class="text-center text-gray-500">
-                                No New Request
-                            </x-table.table-body>
-                        </tr>
-                    @endforelse
-                </x-slot>
-                <div class="px-2 py-2">
-                    {{ $activeUser->links('pagination-links') }}
+                            </x-slot>
+                            <x-slot name="tbody">
+                                @forelse ($activeUser as $index => $lists)
+                                    <tr wire:key="lists-{{ $lists->id }}">
+                                        <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
+                                            <div class="flex space-x-2">
+                                                <p class="block md:hidden">No:</p>
+                                                <p>{{ $loop->iteration  }}</p>
+                                            </div>
+                                        </x-table.table-body>
+                                        <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
+                                            <div class="flex space-x-2">
+                                                <p class="block md:hidden">Name:</p>
+                                                <p>{{ $lists->user->name }}</p>
+                                            </div>
+                                        </x-table.table-body>
+                                        <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
+                                            <div class="flex space-x-2">
+                                                <p class="block md:hidden">Email:</p>
+                                                <p>{{ $lists->user->email }}</p>
+                                            </div>
+                                        </x-table.table-body>
+                                        <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
+                                            <div class="flex space-x-2">
+                                                <p class="block md:hidden">Contact No:</p>
+                                                @if($lists->user->role == 3)
+                                                    <p>{{ $lists->user->profile->phone1 }}</p>
+                                                @elseif ($lists->user->role == 4)
+                                                    <p>{{ $lists->user->phone_no }}</p>
+                                                @endif
+                                            </div>
+                                        </x-table.table-body>
+                                        @if (auth()->user()->role != 1)
+                                            <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
+                                                <div class="flex space-x-2">
+                                                    <p class="block md:hidden">Membership ID:</p>
+                                                    <p>{{ $lists->user->profile->membership_id }}</p>
+                                                </div>
+                                            </x-table.table-body>
+                                        @elseif(auth()->user()->role == 1)
+                                            <x-table.table-body colspan="" class="text-xs font-medium text-gray-700 ">
+                                                <div class="flex space-x-2">
+                                                    <p class="block md:hidden">Referral Code:</p>
+                                                    @if ($lists->user->referralCode == NULL)
+                                                        <div class="flex">
+                                                            <button wire:click="generate({{ $lists->user->id }})" class="flex items-center justify-center px-2 py-2 text-white bg-green-400 rounded-lg hover:bg-green-300">
+                                                                <x-heroicon-o-key class="w-6 h-6 "/>
+                                                                <p class="ml-2 font-bold">Generate</p>
+                                                            </button>
+                                                        </div>
+                                                    @else
+                                                        <p>{{ $lists->user->referralCode->referral_code }}</p>
+                                                    @endif
+                                                </div>
+                                            </x-table.table-body>
+                                        @endif
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <x-table.table-body colspan="4" class="text-center text-gray-500">
+                                            No New Request
+                                        </x-table.table-body>
+                                    </tr>
+                                @endforelse
+                            </x-slot>
+                            <div class="px-2 py-2">
+                                {{ $activeUser->links('pagination-links') }}
+                            </div>
+                        </x-table.table>
+                    </div>
                 </div>
-            </x-table.table>
+            </div>
         </div>
     </div>
 </div>
